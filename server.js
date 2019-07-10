8daa2e551af5b7468055f1cdad0e2c6274702f29
Memou <equaco@gmail.com>
2019-07-10 01:54:00 -0400

    backend setup done with express,mongoose and connected to atlas

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
-
+const connectDb = require('./config/db');
 const app = express();
 
+//Connect Database
+connectDb();
+app.use(express.json({ extended: false }));
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API...' })
 );
 
 // Define Routes
 
-app.use('/api/users',require('./routes/users'));
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/contacts',require('./routes/contacts'));
-
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
