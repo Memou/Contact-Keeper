e1e74ad8ab8b49b88b97573b7ebe35fb51ca8f64
Memou <equaco@gmail.com>
2019-07-14 03:06:03 -0400

    Edit update done on state,clear and current done

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import ContactState from './context/contact/ContactState';
+
+const App = () => {
+  return (
+    <ContactState>
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+    </ContactState>
+
+  );
+};
+
+export default App;
